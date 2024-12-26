2a936a762f02f90d6118b903ca26239cc1f275a3
Renato28 <renatonobrega87@gmail.com>
2024-12-26 07:58:31 -0300

    projeto criado

---
+++ src/main/java/io/github/renatonobrega/aulao_spring_security/AulaoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.github.renatonobrega.aulao_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaoSpringSecurityApplication.class, args);
+	}
+
+}
