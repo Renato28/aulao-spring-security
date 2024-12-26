51265ceb491f48eff20de662dac234060b7642ae
Renato28 <renatonobrega87@gmail.com>
2024-12-26 08:47:34 -0300

    implementando a classe AuthenticationService

---
+++ src/main/java/io/github/renatonobrega/aulao_spring_security/security/AuthenticationService.java
@@ -0,0 +1,17 @@
+package io.github.renatonobrega.aulao_spring_security.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private JwtService jwtService;
+
+    public AuthenticationService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String authenticate(Authentication authentication) {
+        return jwtService.generateToken(authentication);
+    }
+}
